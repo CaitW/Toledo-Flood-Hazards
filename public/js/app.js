eaeb92e1faf924bc87e34c6e872a13b8d4f4ef3a
CaitW <wolters.caitlin@gmail.com>
2016-12-05 18:51:02 +0700

    Fixed legend relating to monetary loss display

--- public/js/app.js
+++ public/js/app.js
@@ -1636,7 +1636,17 @@ function makeTicks() {
         .transition()
         .duration(500)
         .text(function(d) {
-            return (breaks[d + 1] != undefined) ? (d3.round(breaks[d]) + ' - ' + (d3.round(breaks[d + 1]) - 1) + '%') : (d3.round(breaks[d]) + "% +")
+            if(getCurrentAttribute() == 'BldgLossUS') {
+                var ret = "";
+                var notLastValue = breaks[d + 1] != undefined;
+                if(notLastValue) {
+                    return '< $' + d3.round(breaks[d + 1]).toString();
+                } else {
+                    return '$' + d3.round(breaks[d]).toString() + '+';
+                }
+            } else {
+                return (breaks[d + 1] != undefined) ? (d3.round(breaks[d]) + ' - ' + (d3.round(breaks[d + 1]) - 1) + '%') : (d3.round(breaks[d]) + "% +");
+            }
         });
     d3.selectAll('.legendHeading')
         .transition()
@@ -1795,7 +1805,7 @@ function getDGIndex() {
 }
 
 function getCurrentAttribute() {
-    return $('#fieldSelector option:selected')
+    return $("input[name='fieldRadios']:checked")
         .val();
 }
 
@@ -3576,9 +3586,8 @@ function init() {
     //////////////////////
     $('input[name="fieldRadios"]')
         .on('change', function() {
-            currentAttribute = $(this)
-                .val()
-            handleStyle()
+            currentAttribute = getCurrentAttribute();
+            handleStyle();
         })
     $('#fieldSelector')
         .on("change", function() {
